28ff00957aa7bb219e52f81191380d38eb62f800
LeviHernandez316 <76029694+LeviHernandez316@users.noreply.github.com>
2024-08-20 11:01:41 -0600

    arreglos en el backend

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -8,7 +8,14 @@ const obtenerUsuarios = async (req, res) =>{
         const connection = await getConection.conector();
         const [rows,fields] = await connection.execute('select * from Usuario where Correo = ? and contraseña = ?', [correo, contraseña]);
         console.log(rows);
-        res.status(200).json(rows);
+
+        if (rows.length > 0) {
+            res.status(200).json({ message: 'Login successful' });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // res.status(200).json(rows);
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Error en el servidor' });
--- backend/routers/usuarios.router.js
+++ backend/routers/usuarios.router.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var usuarioController = require('../controllers/usuario.controller')
 
-router.get('/', usuarioController.obtenerUsuarios);
+router.post('/', usuarioController.obtenerUsuarios);
 
 // router.get('/', usuarioController.obtenerUsuarios);
 // router.post('/', usuarioController.insertarPersona);
