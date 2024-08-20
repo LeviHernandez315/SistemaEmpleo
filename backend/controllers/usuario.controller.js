9eba52c2a2c3e129fc73c3665bad9ed3b8708e19
LeviHernandez316 <76029694+LeviHernandez316@users.noreply.github.com>
2024-08-19 20:32:48 -0600

    actualizar el gitgub

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -9,6 +9,8 @@ const obtenerUsuarios = async (req, res) =>{
     } catch (err) {
         console.error(err);
         res.status(500).json({ message: 'Error en el servidor' });
+
+        
     }
     
     
